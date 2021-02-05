e3b8b3e006641d82c226be723a8a7ef0c03c4154
Mohammed Hossain <jamalhossain1629@gmail.com>
2021-02-05 10:08:10 -0500

    First Draft

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -32,17 +32,37 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
-
+            var vehicles = new List<Vehicle>();
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
              * Set the properties with object initializer syntax
              */
 
+            Car focus = new Car() { HasTrunk = true, Make = "ford", Modle = "5S", Year = "2000" };
+            Motorcycle motorcycle = new Motorcycle() { HasSideCart = true, Make = "HD", Modle = "Chopper", Year = "1987" };
+
+            Vehicle sedan = new Car() { Make = "dodge", Modle = "blah", Year = "2013" };
+            Vehicle sport = new Car() { HasTrunk = true, Make = "Ferari", Modle = "Hero", Year = "2000" };
+
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+
+            vehicles.Add(focus);
+            vehicles.Add(motorcycle);
+            vehicles.Add(sedan);
+            vehicles.Add(sport);
+
+            foreach(var veh in vehicles)
+            {
+                Console.WriteLine($" Make {veh.Make} Model {veh.Modle} Year {veh.Year}");
+                veh.DriveAbstract();
+                Console.WriteLine( "--------------------------------------------");
+            }
+
+
 
             // Call each of the drive methods for one car and one motorcycle
 
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+   abstract class Vehicle
+    {
+        public string Year { get; set; } = "2020";
+        public string Make { get; set; } = "Default";
+        public string Modle { get; set; } = "Default";
+
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($" This {GetType().Name} is virtual in drive");
+        }
+    }
+}
